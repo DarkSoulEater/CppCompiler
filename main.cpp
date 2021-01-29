3f6921a71bac07bc4bdfa9acca82fd8e5dda8464
Artyom Plevako <artyomplevako@yandex.ru>
2021-01-29 10:23:22 +0300

    Node

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ int main(int argc, char *argv[]) {
   LexicalAnalyzer lexical_analyzer(argv[1]);
   while (!lexical_analyzer.Empty()) {
     auto token = lexical_analyzer.Get();
-    std::cout << token.value << " - " << static_cast<int>(token.name) << '\n';
+    std::cout << token.value << " - ";
+    switch (static_cast<int>(token.name)) {
+      case 0: std::cout << "KEYWORD"; break;
+      case 1: std::cout << "IDENTIFIER"; break;
+      case 2: std::cout << "CONSTANT"; break;
+      case 3: std::cout << "STRING_LITERAL"; break;
+      case 4: std::cout << "PUNCTUATOR"; break;
+      case 5: std::cout << "OPERATOR"; break;
+      case 6: std::cout << "OTHER"; break;
+    }
+    std::cout << '\n';
   }
 }
